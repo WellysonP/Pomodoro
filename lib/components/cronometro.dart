8eed50d9f84becf7477693896b772d0273ab3935
Wellyson Pontes <wellysonpontes@gmail.com>
2022-06-27 17:12:35 -0300

    criado mobex

--- lib/components/cronometro.dart
+++ lib/components/cronometro.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:pomodoro/components/cronometro_botao.dart';
 
 class Cronometro extends StatelessWidget {
   const Cronometro({Key? key}) : super(key: key);
@@ -13,21 +16,49 @@ class Cronometro extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
+          const Text(
             "Hora de Trabalhar",
             style: TextStyle(
               fontSize: 40,
               color: Colors.white,
             ),
           ),
-          SizedBox(height: 20),
-          Text(
+          const SizedBox(height: 20),
+          const Text(
             "25:00",
             style: TextStyle(
               fontSize: 120,
               color: Colors.white,
             ),
-          )
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: const CronometroBotao(
+                  texto: "Iniciar",
+                  icone: Icons.play_arrow,
+                ),
+              ),
+              // Padding(
+              //   padding: const EdgeInsets.only(right: 10),
+              //   child: CronometroBotao(
+              //     texto: "Parar",
+              //     icone: Icons.stop,
+              //   ),
+              // ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: const CronometroBotao(
+                  texto: "Reiniciar",
+                  icone: Icons.refresh,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
---
+++ lib/components/cronometro_botao.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CronometroBotao extends StatelessWidget {
+  final String texto;
+  final IconData icone;
+  const CronometroBotao({
+    Key? key,
+    required this.texto,
+    required this.icone,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.black,
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+        textStyle: TextStyle(fontSize: 25),
+      ),
+      onPressed: () {},
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(icone, size: 30),
+          ),
+          Text(texto),
+        ],
+      ),
+    );
+  }
+}
