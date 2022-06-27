d7ff0e9746649d9ebfa79191eed50fe460732655
Wellyson Pontes <wellysonpontes@gmail.com>
2022-06-27 15:21:26 -0300

    feito tela de trabalho

--- lib/pages/pomodoro_page.dart
+++ lib/pages/pomodoro_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:pomodoro/pages/entrada_tempo.dart';
+import 'package:pomodoro/components/cronometro.dart';
+import 'package:pomodoro/components/entrada_tempo.dart';
 
 class PomodoroPage extends StatelessWidget {
   const PomodoroPage({Key? key}) : super(key: key);
@@ -10,20 +11,26 @@ class PomodoroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text("Podomoro"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              EntradaTempo(
-                titulo: "Trabalho",
-                valor: 25,
-              ),
-              EntradaTempo(
-                titulo: "Descanso",
-                valor: 5,
-              ),
-            ],
+          Expanded(
+            child: Cronometro(),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                EntradaTempo(
+                  titulo: "Trabalho",
+                  valor: 25,
+                ),
+                EntradaTempo(
+                  titulo: "Descanso",
+                  valor: 5,
+                ),
+              ],
+            ),
           ),
         ],
       ),
